1ba50bd4d39051d6842d14dd6dc3173bb7872725
PNS212624\Hansol <yrlee00@hansol.com>
2024-10-21 15:41:27 +0900

    10/21 theme typescript 실습

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "white",
+    textColor: "black",
+    btnColor: "tomato",
+};
+
+export const dartTheme: DefaultTheme = {
+    bgColor: "black",
+    textColor: "white",
+    btnColor: "teal"
+}
