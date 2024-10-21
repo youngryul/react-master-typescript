d6dc3290f09b9d76d264e845d429d294b64a4484
PNS212624\Hansol <yrlee00@hansol.com>
2024-10-21 16:04:18 +0900

    10/21 react-router-dom 6버전

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, {useState} from 'react';
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};  
-`;
 
 function App() {
     return (
-        <Container>
-            <H1>protected</H1>
-        </Container>
+       <div>
+           <Router />
+       </div>
     );
 
 }
